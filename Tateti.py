4f73f6435a3d20876d8c3b489728c0c9813b5839
MauricioBrynkier <mauricio.brynkier@gmail.com>
2022-04-01 01:40:24 -0300

    Excepciones
    Se agrego excepciones para evitar que el juego no se cierre

--- Tateti.py
+++ Tateti.py
@@ -1,14 +1,22 @@
 import os
+import sys
+
+class Error(Exception):
+    #clase base para excepciones
+    pass
+class PosicionOcupada(Error):
+    #Error creado para no pisar una posicion ya elegida
+    pass
 
 def display_board(choice,pos,player_1):
     
     
     if choice in range(1,10):
         if not player_1:
-            pos[int(choice)-1] ='X'
+            pos[choice-1] ='X'
             
         else:
-            pos[int(choice)-1] ='O'
+            pos[choice-1] ='O'
             
     
     print("       |       |")
@@ -61,8 +69,23 @@ def game_logic():
         while choice not in range(1,10):
             
             if player_1:
-                
-                choice = int(input('Que el jugador 1 elija una posicion del 1 al 9: '))
+
+                while True:
+                    try:
+                        choice = int(input('Que el jugador 1 elija una posicion del 1 al 9: '))
+                        if pos[choice-1] == 'X' or pos[choice-1] == 'O':
+                            raise PosicionOcupada
+                    except ValueError:
+                        os.system('cls')
+                        display_board(choice,pos,player_1)
+                        print('Ops! Parece que ingreso algo distinto a un numero. Por favor vuelva a intentarlo.')
+                    except PosicionOcupada:
+                        os.system('cls')
+                        display_board(choice,pos,player_1)
+                        print("Ops! Esa posicion ya se encuentra ocupada. Por favor elija otra")
+                    else:
+                        break
+                    
                            
                 if choice not in range(1,10):
                     print('posicion erronea, por favor vuelve a intentarlo')
@@ -72,7 +95,21 @@ def game_logic():
                              
                              
             else:
-                choice = int(input('Que el jugador 2 elija una posicion del 1 al 9: '))
+                while True:
+                    try:
+                        choice = int(input('Que el jugador 2 elija una posicion del 1 al 9: '))
+                        if pos[choice-1] == 'X' or pos[choice-1] == 'O':
+                            raise PosicionOcupada
+                    except ValueError:
+                        os.system('cls')
+                        display_board(choice,pos,player_1)
+                        print('Ops! Parece que ingreso algo distinto a un numero. Por favor vuelva a intentarlo.')
+                    except PosicionOcupada:
+                        os.system('cls')
+                        display_board(choice,pos,player_1)
+                        print("Ops! Esa posicion ya se encuentra ocupada. Por favor elija otra")
+                    else:
+                        break
                              
                 if choice not in range(1,10):
                     print('posicion erronea, por favor vuelve a intentarlo')
